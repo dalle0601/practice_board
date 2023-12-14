5324b465fbe108fe581e77291124d3c7b992a70e
dalle0601 <dalle0601@naver.com>
2023-12-14 18:20:01 +0900

    231214

--- practice_board/src/main/java/com/example/demo/domain/Article.java
+++ practice_board/src/main/java/com/example/demo/domain/Article.java
@@ -1,14 +1,36 @@
 package com.example.demo.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;   // 제목
-    private String hashtag; // 내용
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Setter private String title;   // 제목
+    @Setter private String content; // 본문
+    @Setter private String hashtag; // 해시태그
+
+    @CreatedDate private LocalDateTime createdAt; // 생성일시
+    @CreatedBy private String createdBy; // 생성자
+    @LastModifiedDate private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy private String modifiedBy; // 수정자
 }
